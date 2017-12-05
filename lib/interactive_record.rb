2e2386fe094a9d2d01dd80c81560005a86a13297
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-12-05 01:20:39 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,7 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute_obj)
-    sql = "SELECT * FROM #{self.table_name} WHERE #{} = '#{}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{key} = '#{value}'"
     row = DB[:conn].execute(sql)
   end
 
